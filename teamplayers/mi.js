139cdff6e3edf207fe7c356877dba2df773443ec
soumya-1234 <43286107+soumya-1234@users.noreply.github.com>
2022-03-13 18:48:16 +0530

    Add files via upload

---
+++ teamplayers/mi.js
@@ -0,0 +1,30 @@
+function Rohit() {
+    document.getElementById('rohit').innerHTML = `<h1>Rohit Sharma</h1><br></br><p>Team- Mumbai Indians</p>
+          <p>Price- 16Corore</p>
+          <p>Playing Status- Playing</p>
+          <p>Role- Batsmen</p><br></br><br></br>
+          <p>He is a right-handed batter and a right arm orthodox spinner. He played for the Deccan Chargers from 2008-2010 before joining us, the Mumbai Indians in the 2011 auction. He has since gone on to become our captain, winning 5 IPL Titles and 2 Champions League titles at the helm. He is India's current white-ball captain.</p>`;
+ }
+ function Pollard() {
+    document.getElementById('pollard').innerHTML=`<h1>
+          Kieron Pollard</h1><br></br> <p>Team- Mumbai Indians</p>
+          <p>Price- 10Corore</p>
+          <p>Playing Status- Playing</p>
+          <p>Role- All Rounder</p><br></br><br></br> <p>West Indies all-rounder Kieron Pollard joined the Mumbai Indians squad in 2010, and since then, he has won five titles with the team. Also, he has been retained by MI ahead of the Indian Premier League (IPL) 2022 auctions for Rs 10 crore. </p> `;
+ }
+ function Bumrah(){
+    document.getElementById('bumrah').innerHTML=`<h1>
+             Jusprit Bumrah</h1><br></br> <p>Team- Mumbai Indians</p>
+          <p>Price- 12Corore</p>
+          <p>Playing Status- Playing</p>
+          <p>Role- Bowler</p><br></br><br></br>
+          <p>Jasprit Jasbirsingh Bumrah is an Indian international cricketer, who plays for the Indian national cricket team in all formats of the game. He is the first Asian bowler to take 5 wickets in a test innings in South Africa, England and Australia during the same calendar year.</p>`;
+ }
+ function Anmol(){
+    document.getElementById('Anmol').innerHTML=`<h1>
+             Anmol Preet Singh</h1><br></br><p>Team- Mumbai Indians</p>
+          <p>Price- 5Corore</p>
+          <p>Playing Status- On-bench</p>
+          <p>Role- Batsmen</p><br></br><br></br>
+           <p>Anmolpreet Singh (born 28 March 1998) is an Indian professional cricketer who plays for Punjab. A right-handed batsman, he is a part of the Mumbai Indians squad in the IPL. He made his IPL debut on 19th September 2021 against Chennai Super Kings.</p>`;
+ }
